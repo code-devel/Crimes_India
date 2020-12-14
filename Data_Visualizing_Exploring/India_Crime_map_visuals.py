4dd4c43d19cb4fb6e5d5189c41d71f6786cb1107
sagar <singh14sagar@gmail.com>
2020-12-14 14:03:10 +0530

    updates, "India_Crime_map_Visuals" now working

--- Data_Visualizing_Exploring/India_Crime_map_visuals.py
+++ Data_Visualizing_Exploring/India_Crime_map_visuals.py
@@ -1,6 +1,12 @@
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# ----------------Running this file creates the geographical graphs of the crimes-------------------
+
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 import pandas as pd 
 import geopandas as gpd
-from pandas.core.reshape.merge import merge 
+
 import matplotlib.pyplot as plt 
 
 def readFile(filename):
@@ -64,8 +70,9 @@ df = readFile(filename)
 # print(df.head(5))
 
 
-fp = "D:/__studymaterial__/7-sem/Capston Project/Crimes_India/Data_Visualizing_Exploring/data/India_Geograph/India States/Indian_states.shp"
+fp="D:/__studymaterial__/7-sem\Capston Project/Crimes_India/Data_Visualizing_Exploring/data/India_Geograph/India States/Indian_States.shp"
 map_df = gpd.read_file(fp)
+
 
 merged = map_df.set_index('st_nm').join(df.set_index('Area_Name'))
 # print(merged.head())
@@ -78,11 +85,4 @@ print("-----/ Enter the Crime No. to get the result \-----\n\
     4.Total_Murders,\n\
     5.Total_Kidnapp")
 Input_crime_name = int(input("Enter The crime index:- "))
-a = MapPlot(merged,Input_crime_name)
-
-# fig, ax = plt.subplots(1,figsize=(10,6))
-# ax.axis('off')
-# ax.set_title('State Wise Crime against Women in India', fontdict={'fontsize':'25', 'fontweight':'3'})
-
-# merged.plot(column='Victims_of_Rape_Total',cmap='YlOrRd',linewidth=0.8, ax=ax, edgecolor='0.8',legend=True)
-# plt.show()
+MapPlot(merged,Input_crime_name)
